c179d83a64d0d87640cb20b568955d16e1a365cd
Quadra <monichaa@mer.ci.nsu.ru>
2024-10-20 19:04:44 +0300

    fixed: #1 header design

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,26 +1,16 @@
 import { memo } from 'react';
 
-import Satellite from '@/shared/assets/header/satellite.png';
-import Lake from '@/shared/assets/header/lake.png';
-
 import * as s from './Header.module.scss';
 
 const MemoHeader = () => {
 	return (
 		<div className={s.header}>
-			<div className={s.header__image}>
-				<img src={Satellite} alt="satellite" />
-			</div>
 			<div className={s.header__title}>
 				<span>
 					{'ИС анализа Байкальской природной'}
 					<br />
 					{' зоны с помощью спутниковых снимков'}
 				</span>
-			</div>
-
-			<div className={s.header__image}>
-				<img src={Lake} alt="lake" />
 			</div>
 		</div>
 	);
