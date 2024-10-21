c3316ef130f11186484860ab17773aeb27c3c1b1
Денис Голопапа <golopapa@kos-com.ru>
2024-10-21 23:04:49 +0400

    improvement: #1 added react HMR for development

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,6 +10,8 @@ interface IEnvVariables {
 }
 
 export default (env: IEnvVariables) => {
+	const isDev = env.mode === 'development';
+
 	const paths: IBuildPaths = {
 		entry: path.resolve(__dirname, 'src', 'index.tsx'),
 		html: path.resolve(__dirname, 'public', 'index.html'),
@@ -23,6 +25,19 @@ export default (env: IEnvVariables) => {
 		paths,
 		analyzer: env.analyzer || false,
 	});
+
+	if (isDev) {
+		config.devServer = {
+			hot: true, // делаем "горячую перезагрузк"
+			historyApiFallback: true, // для обработки маршрутов
+			static: {
+				directory: paths.output, // подача статических файлов из папки сборки
+			},
+			port: env.port || 8080,
+		};
+
+		config.plugins?.push(new webpack.HotModuleReplacementPlugin());
+	}
 
 	return config;
 };
