1d89aa9594e01cdaab5c454c7c384dab40c68a38
Quadra <monichaa@mer.ci.nsu.ru>
2024-11-06 19:10:51 +0300

    fixed: #0 router import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import { Header } from '@/widgets/header';
-import { AppRouter } from './providers';
+import { AppRouter } from './providers/routers';
 
 import './styles/normalize.scss';
 
 function App() {
-	return <>
-		<Header />
-		<AppRouter />
-	</>;
+	return (
+		<>
+			<Header />
+			<AppRouter />
+		</>
+	);
 }
 
 export { App };
