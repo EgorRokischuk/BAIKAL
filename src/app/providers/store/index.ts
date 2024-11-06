e8b724896b525a8ceb9663d5ccdf620c533174a2
Quadra <monichaa@mer.ci.nsu.ru>
2024-11-06 18:58:07 +0300

    new: #3 added currentPath updating

--- src/app/providers/store/index.ts
+++ src/app/providers/store/index.ts
@@ -1,2 +1,3 @@
 export { createReduxStore, type RootState, type AppDispatch } from './config';
 export { StoreProvider } from './ui/StoreProvider';
+export { globalActions } from './model/globalReducer';
