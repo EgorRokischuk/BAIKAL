1a7955a3ed4c950c9e892baab7c21f3fc44586c6
Quadra <monichaa@mer.ci.nsu.ru>
2024-11-17 12:27:33 +0300

    new: #6 drag control

---
+++ src/shared/ui/DragControl.tsx
@@ -0,0 +1,20 @@
+import { useMapDragging } from '@/shared/hooks/useMapDragging';
+import { PropsWithChildren } from 'react';
+import { useMap } from 'react-leaflet';
+
+interface IProps {
+	className: string;
+}
+
+function DragControl({ className, children }: PropsWithChildren<IProps>) {
+	const map = useMap();
+	const { enableMapDragging, disableMapDragging } = useMapDragging(map);
+
+	return (
+		<div className={className} onMouseLeave={enableMapDragging} onMouseDown={disableMapDragging}>
+			{children}
+		</div>
+	);
+}
+
+export { DragControl };
