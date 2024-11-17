d8f2fe9a8d9853996c35716e7790807066323249
Quadra <monichaa@mer.ci.nsu.ru>
2024-11-17 12:30:25 +0300

    new: #6 change zoom feature

--- src/widgets/map/ui/Map.tsx
+++ src/widgets/map/ui/Map.tsx
@@ -1,10 +1,12 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import { useAppSelector } from '@/shared/hooks/useAppSelector';
+import { ChangeZoom } from '@/features/Map/change-zoom';
+import { getMapZoom } from '@/entities/Map';
+
 import { LAYER_LINK, MAP_PROPS } from '../config/constants';
 import './Map.css';
-import { useAppSelector } from '@/shared/hooks/useAppSelector';
-import { getMapZoom } from '@/entities/Map';
 
 const Map = () => {
 	const zoom = useAppSelector(getMapZoom);
@@ -12,6 +14,8 @@ const Map = () => {
 	return (
 		<MapContainer zoom={zoom} zoomControl={false} doubleClickZoom={false} {...MAP_PROPS}>
 			<TileLayer url={LAYER_LINK} />
+
+			<ChangeZoom />
 		</MapContainer>
 	);
 };
