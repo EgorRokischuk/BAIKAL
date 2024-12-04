d4f39e143b359d3972534ce2191e4a0d7f50def7
Денис Голопапа <rediskink@inbox.ru>
2024-12-04 14:12:06 +0400

    fixed: #0 date picker

--- src/entities/map/ui/tile-date-picker/ui/TileDatePicker.tsx
+++ src/entities/map/ui/tile-date-picker/ui/TileDatePicker.tsx
@@ -2,20 +2,45 @@ import { useAppDispatch } from '@/shared/hooks/useAppDispatch';
 import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import { getTileDate } from '@/entities/map/model/selectors';
 import { mapActions } from '@/entities/map/model/slices';
-import { DatePicker, DatePickerProps } from '@mui/x-date-pickers';
+import { DatePicker, DatePickerProps, LocalizationProvider } from '@mui/x-date-pickers';
 import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/ru';
+dayjs.locale('ru');
 
 const TileDatePicker = (props: DatePickerProps<Dayjs, false>) => {
 	const date = useAppSelector(getTileDate);
 	const dispatch = useAppDispatch();
 
 	return (
-		<DatePicker
-			{...props}
-			label="День"
-			value={!!date ? dayjs(date, 'DD_MM_YY') : null}
-			onChange={(date) => dispatch(mapActions.setDate(date.toISOString()))}
-		/>
+		<LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+			<DatePicker
+				{...props}
+				label="День"
+				value={!!date ? dayjs(date, 'DD_MM_YY') : null}
+				onChange={(date) =>
+					dispatch(mapActions.setDate(date ? date.toISOString() : null))
+				}
+				slotProps={{
+					textField: {
+						sx: {
+							'& .MuiInputBase-input': {
+								fontSize: '16px', // Размер текста в поле ввода
+							},
+						},
+					},
+					desktopPaper: {
+						sx: {
+							'& .MuiPickersDay-root': {
+								fontSize: '15px',
+								height: '40px',
+								width: '40px', 
+							},
+						},
+					},
+				}}
+			/>
+		</LocalizationProvider>
 	);
 };
 
