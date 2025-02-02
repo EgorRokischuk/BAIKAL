ebd2346b2fa376554fe6a3a39bc88ee02b04889c
Quadra <monichaa@mer.ci.nsu.ru>
2025-02-02 12:04:41 +0300

    new: #0 panel component

--- src/widgets/map-menu/ui/MapMenu.tsx
+++ src/widgets/map-menu/ui/MapMenu.tsx
@@ -1,48 +1,21 @@
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
-import { Button, Collapse, Typography } from '@mui/material';
-import InfoIcon from '@mui/icons-material/Info';
-import { useState } from 'react';
-
-import * as s from './MapMenu.module.scss';
 import { MENU_STRUCTURE } from '../config/constants';
 import { TabMenu } from '@/entities/Map';
 import { ShowTile } from '@/features/Map/show-tile';
 import { DownloadTile } from '@/features/Map/download-tile';
+import { Panel } from '@/shared/ui/Panel';
 
 const MapMenu: React.FC = () => {
-	const [collapsed, setCollapsed] = useState(false);
-
 	return (
-		<>
-			<Collapse in={collapsed}>
-				<ArrowForwardIos onClick={() => setCollapsed(false)} />
-			</Collapse>
+		<Panel header="Продукты">
+			<Panel.Content>
+				<TabMenu tabs={MENU_STRUCTURE} />
+			</Panel.Content>
 
-			<Collapse in={!collapsed}>
-				<div className={s.container}>
-					{/* Menu Header */}
-					<div className={s.header}>
-						<ArrowBackIos />
-						<Typography variant="map_menu_label" align="center">
-							{'Продукты'}
-						</Typography>
-						<div />
-					</div>
-
-					{/* Menu Content */}
-					<TabMenu tabs={MENU_STRUCTURE} />
-
-					{/* Menu Footer (with features) */}
-					<div className={s.footer}>
-						<DownloadTile />
-						<ShowTile />
-						<Button>
-							<InfoIcon />
-						</Button>
-					</div>
-				</div>
-			</Collapse>
-		</>
+			<Panel.Actions>
+				<DownloadTile />
+				<ShowTile />
+			</Panel.Actions>
+		</Panel>
 	);
 };
 
