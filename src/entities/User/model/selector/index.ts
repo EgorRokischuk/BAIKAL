99c0b227bc43c58f89a91884b5ffeed4bef713b2
Aleksandr Monich <aleksandr.monich@sgs.com>
2025-04-20 19:01:05 +0700

    improvement: #9 edited visibility of create button

--- src/entities/User/model/selector/index.ts
+++ src/entities/User/model/selector/index.ts
@@ -1,3 +1,5 @@
 import { IState } from '@/shared/config/store/State';
 
 export const getFullProfile = (state: IState) => state.user.fullProfile;
+
+export const getUserRights = (state: IState) => state.user.fullProfile.userRights;
--- src/pages/external-resources/ui/ExternalResources.tsx
+++ src/pages/external-resources/ui/ExternalResources.tsx
@@ -1,8 +1,11 @@
 import { Box } from '@mui/material';
 import { CreateEntity } from '@/features/create-entity';
+import { getUserRights } from '@/entities/User';
+import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import * as s from './ExternalResources.module.scss';
 
 const ExternalResources: React.FC = () => {
+	const userRights = useAppSelector(getUserRights);
 	return (
 		<Box className={s.page}>
 			<Box>
@@ -11,8 +14,11 @@ const ExternalResources: React.FC = () => {
 
 			<Box className={s.page__gallery}>{'External Resources'}</Box>
 
-			<Box className={s.page__block}>
-				<CreateEntity type="external-resource" />
+			<Box>
+				{userRights.some((role) => role.name === 'admin') && (
+					<CreateEntity type="external-resource" />
+				)}
+				<Box className={s.page__block} />
 			</Box>
 		</Box>
 	);
