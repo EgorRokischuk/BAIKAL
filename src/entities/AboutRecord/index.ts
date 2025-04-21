e5ed55d4119b21cc2329cb9c15602a4debe747cf
Aleksandr Monich <aleksandr.monich@sgs.com>
2025-04-21 23:10:52 +0700

    new: #11 init about record entity

---
+++ src/entities/AboutRecord/index.ts
@@ -0,0 +1,9 @@
+export { aboutRecordHandlers } from './api/__mocks__';
+export {
+	aboutRecordApi,
+	useGetAboutRecordsListQuery,
+	useGetAboutRecordByIdQuery,
+	useCreateAboutRecordMutation,
+	useUpdateAboutRecordMutation,
+	useDeleteAboutRecordMutation,
+} from './api/aboutRecordApi';
