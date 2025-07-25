950b42f07ebf8baf471c0aacc34383a3fb0b7bbb
Aleksandr Monich <aleksandr.monich@sgs.com>
2025-07-25 10:09:27 +0700

    fixed: #16 auth mocks

--- src/entities/user/api/__mocks__/data/mockLoginResponse.ts
+++ src/entities/user/api/__mocks__/data/mockLoginResponse.ts
@@ -1,15 +1,5 @@
 export default {
-	accessToken: 'accessToken',
-	user: {
-		id: 'baed980f-9f83-4d4c-aa0c-74a933c2b9cd',
-		fullname: 'Ivanov Ivan Ivanovich',
-		email: 'admin@mail.ru',
-		avatarUrl: 'example.com',
-		userRights: [
-			{
-				id: 'baed980f-9f83-4d4c-aa0c-74a933c2b9ee',
-				name: 'admin',
-			},
-		],
-	},
+	access_token: 'accessToken',
+	refresh_token: 'refreshToken',
+	token_type: 'bearer',
 };
