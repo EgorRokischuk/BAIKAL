973257e7a50771c8f476a04e83ccb48694038f34
veeelzs <SokolovaVA@mer.ci.nsu.ru>
2025-12-13 23:48:13 +0700

    fixed: footer & map menu position

--- src/widgets/map-menu/ui/MapMenu.tsx
+++ src/widgets/map-menu/ui/MapMenu.tsx
@@ -4,21 +4,26 @@ import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import { Panel } from '@/shared/ui/Panel';
 import { MENU_STRUCTURE } from '../config/constants';
 import { MenuActions } from './MenuActions';
+import * as s from './MapMenu.module.scss';
 
 const MapMenu: React.FC = () => {
 	const isLoading = useAppSelector(({ global }) => global.isLoading);
 
 	return (
-		<Panel header="Данные" width="355px">
-			{isLoading && <LinearProgress color="primary" />}
-			<Panel.Content>
-				<TabMenu tabs={MENU_STRUCTURE} />
-			</Panel.Content>
+		<div className={s.menu_container}>
+			<Panel header="Данные" width="355px">
+				{isLoading && <LinearProgress color="primary" />}
+				<Panel.Content>
+					<div className={s.menu_content}>
+						<TabMenu tabs={MENU_STRUCTURE} />
+					</div>
+				</Panel.Content>
 
-			<Panel.Actions>
-				<MenuActions />
-			</Panel.Actions>
-		</Panel>
+				<Panel.Actions>
+					<MenuActions />
+				</Panel.Actions>
+			</Panel>
+		</div>
 	);
 };
 
