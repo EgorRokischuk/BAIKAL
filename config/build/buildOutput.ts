fa8ab77d151adcb6278971a9ef9fed849f7e96a9
EgorRokischuk <153460993+EgorRokischuk@users.noreply.github.com>
2026-02-16 18:24:26 +0700

    Delete config/build directory

--- config/build/buildOutput.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration } from 'webpack';
-import { IBuildOptions } from './types';
-
-export const buildOutput = ({ paths }: IBuildOptions): Configuration['output'] => ({
-	path: paths.output,
-	filename: 'bundle.[contenthash].js',
-	clean: true,
-	publicPath: '/',
-});
